119984e380528c9949ff7f0e9a5c47094ef8af6b
Gabriel Soares <gabrielrs.job@gmail.com>
2023-12-26 16:43:22 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>EDITOR POC</h1>
+  <textarea :value="input" @input="updateMarkdown($event.target.value)"></textarea>
+  <div v-html="compiledMarkdown"></div>
+</template>
+
+<script setup>
+import { marked } from 'marked';
+import { computed, ref } from 'vue';
+
+const compiledMarkdown = computed(() => marked(input.value, {sanitize: true}));
+const input = ref('');
+
+const content = ref('');
+
+function updateMarkdown(event) {
+  input.value = event;
+  console.log("CHAMA", input.value)
+}
+</script>
